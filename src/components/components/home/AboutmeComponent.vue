d0706100c4b87507be457215b46ed17d8576de27
santiago ceballos <santiceballos44@outlook.com>
2025-09-09 22:57:29 -0500

    feat(banner): :lipstick: diseño banner principal

---
+++ src/components/components/home/AboutmeComponent.vue
@@ -0,0 +1,10 @@
+<template>
+	<!-- TODO: seccion de quienes somos -->
+	<!-- TODO: imagen del negocio o de la seccion-->
+	<!-- TODO: informacion de quienes somos y botones de informacion -->
+	<section></section>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
--- src/components/components/home/BannerComponent.vue
+++ src/components/components/home/BannerComponent.vue
@@ -1,12 +1,10 @@
 <template>
 	<article class="container__banner__hero w-full">
-		<section
-			class="container__banner relative min-h-[60vh] lg:min-h-[40vh] h-[70vh] overflow-hidden"
-		>
+		<section class="container__banner relative">
 			<picture class="w-full container__video__banner">
 				<source srcset="https://placehold.co/400x800" media="(max-width: 1200px)" />
 				<img
-					src="https://placehold.co/1400x700"
+					src="https://placehold.co/1400x600"
 					alt="video kinesica R"
 					class="w-full object-cover"
 				/>
@@ -19,18 +17,24 @@
 				<h1 class="uppercase text-6xl lg:text-[5vw] font-bold mb-10">
 					SIN EXCUSAS. SOLO RESULTADOS
 				</h1>
-				<h3 class="mb-20 w-1/2 text-center">
+				<h2 class="mb-20 max-w-[70rem] text-center text-4xl">
 					Recuperación atlética de élite. Maximiza tu rendimiento deportivo con técnicas
 					especializadas en fisioterapia deportiva.
-				</h3>
-				<section class="container__button__banner flex gap-8 lg:gap-10 flex-wrap">
+				</h2>
+				<section
+					class="container__button__banner flex gap-8 lg:gap-10 flex-wrap mb-20 md:mb-40 justify-center"
+				>
 					<a href="" class="boton-contacto"
 						>Agenda con nosotros <calendar-days-icon class="size-20 inline" />
 					</a>
-					<a href="" class="boton-contacto">
-						Saber Más <ArrowLongRightIcon class="size-10" />
+					<a href="" class="boton-contacto group hover:scale-100">
+						Saber Más
+						<ArrowLongRightIcon
+							class="size-10 rotate-[-45deg] group-hover:scale-150 transition-all"
+						/>
 					</a>
 				</section>
+				<arrow-down-icon class="" />
 			</div>
 		</section>
 	</article>
@@ -39,6 +43,36 @@
 <script setup>
 import { CalendarDaysIcon } from '@heroicons/vue/24/outline';
 import { ArrowLongRightIcon } from '@heroicons/vue/24/outline';
+import ArrowDownIcon from '@/assets/icons/ArrowDownIcon.vue';
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.boton-contacto {
+	padding: 17px 40px;
+	border-radius: 50px;
+	cursor: pointer;
+	border: 0;
+	background-color: white;
+	box-shadow: rgb(0 0 0 / 5%) 0 0 8px;
+	letter-spacing: 1.5px;
+	text-transform: uppercase;
+	font-size: 15px;
+	transition: all 0.5s ease;
+}
+
+.boton-contacto:hover {
+	letter-spacing: 3px;
+	background-color: var(--color-main);
+	color: var(--color-white-app);
+	box-shadow: var(--color-white-main) 0px 7px 29px 0px;
+}
+
+.boton-contacto:active {
+	letter-spacing: 3px;
+	background-color: var(--color-main);
+	color: var(--color-white-app);
+	box-shadow: var(--color-main) 0px 0px 0px 0px;
+	transform: translateY(10px);
+	transition: 100ms;
+}
+</style>

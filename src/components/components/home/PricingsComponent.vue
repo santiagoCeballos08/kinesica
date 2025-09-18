5b098f60ec0f69e6937e2ef581f3f0cdbc13fd87
santiago ceballos <santiceballos44@outlook.com>
2025-09-17 22:13:30 -0500

    feat(pricings): :sparkles: add PricingsComponent to HomeView for pricing display

---
+++ src/components/components/home/PricingsComponent.vue
@@ -0,0 +1,20 @@
+<template>
+	<section class="bg-white container__princings">
+		<section class="container__princings mx-app">
+			<article class="container__princing"></article>
+		</section>
+	</section>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const princings = ref([
+	{
+		title: '',
+		princings: 100,
+		services: {},
+	},
+]);
+</script>
+
+<style scoped></style>
--- src/views/Home/HomeView.vue
+++ src/views/Home/HomeView.vue
@@ -1,9 +1,15 @@
 <script setup>
 import BannerComponent from '@/components/components/home/BannerComponent.vue';
 import AboutmeComponent from '@/components/components/home/AboutmeComponent.vue';
+import PricingsComponent from '@/components/components/home/PricingsComponent.vue';
 </script>
 
 <template>
+	<!-- banner componente -->
 	<banner-component />
+	<!-- componente aboutme -->
 	<aboutme-component />
+
+	<!-- componente de precios -->
+	<pricings-component />
 </template>

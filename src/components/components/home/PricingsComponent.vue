71089e07d14ce342558b1568f2449a6413040ec7
santiago ceballos <santiceballos44@outlook.com>
2025-10-17 23:23:29 -0500

    feat: (precios) :lipstick: creacion de precios

--- src/components/components/home/PricingsComponent.vue
+++ src/components/components/home/PricingsComponent.vue
@@ -1,30 +1,164 @@
 <template>
 	<section class=" container__princings__hero mt-40 bg-white-app p-4">
 		<span class="title-section mb-10">nuestros precios</span>
-		<h2 class="text-title font-bold border-dotted border-b-2 text-center">Nuestros increibles preicios</h2>
+		<h2 class="text-title font-bold border-dotted border-main-300 border-b-2 text-center text-main-700">Nuestros
+			increibles preicios
+		</h2>
 		<section class="container__princings__titles ">
-			<section class="princings__services mt-10 p-5 flex flex-wrap w-full justify-center gap-10">
+			<section class="princings__services mt-10 mb-40 p-5 flex flex-wrap w-full justify-center gap-10">
 				<button class="services__button button "><span> Presoterapia Profesional</span></button>
 				<button class="services__button button "><span>Fisioterapia General</span></button>
 				<button class="services__button button "><span>masoterapia</span></button>
 			</section>
-			<article class="container__princing"></article>
 		</section>
 
 		<!-- precios de proyectos -->
 		<section class="container__princins__cards__hero">
-			<section class="container__princins__cards flex justify-center max-w-app mx-auto">
-				<div class="card-pricings" data-services="">
-					<div class="card__image">
-						<img src="" alt="">
+			<section class="container__princins__cards flex gap-8 justify-center max-w-app mx-auto">
+
+				<!-- CARD DE PRECIOS -->
+				<div class="card-pricings hover:scale-102 transition-all  max-w-[35rem] h-[60rem] bg-linear-45 from-main-50 via-main-50 to-main shadow-md  rounded-4xl p-4"
+					data-services="">
+					<div class="card__info flex flex-col h-full ">
+						<h1 class="text-3xl font-black text-center text-main-600 mt-8">Plan basico</h1>
+						<h2 class="text-6xl font-black text-center text-main-700 mb-8">300.000 Cop</h2>
+						<div
+							class="card__info__meta flex-2 bg-main-600 pt-8 rounded-t-4xl rounded-b-2xl p-8 text-white-app text-[18px]">
+							<p class="text-pretty mb-10 ">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+								Corrupti ab
+								natus mollitia vero, non
+								voluptatem autem unde voluptas et aperiam?</p>
+							<ul class="mb-20">
+								<li>
+									<div class="flex gap-2  items-center">
+										<CheckCircleIcon class="h-8 w-8" />
+										<p>Servicio principal</p>
+									</div>
+								</li>
+								<li>
+									<div class="flex gap-2  items-center">
+										<CheckCircleIcon class="h-8 w-8" />
+										<p>Servicio principal</p>
+									</div>
+								</li>
+								<li>
+									<div class="flex gap-2  items-center">
+										<CheckCircleIcon class="h-8 w-8" />
+										<p>Servicio principal</p>
+									</div>
+								</li>
+								<li>
+									<div class="flex gap-2  items-center">
+										<XCircleIcon class="h-8 w-8" />
+										<p>Servicio principal</p>
+									</div>
+								</li>
+
+							</ul>
+
+							<div class="card__info__footer flex justify-center">
+								<a href="#" class="botton__link flex gap-2">Obtener plan
+									<ArrowUpRightIcon class="h-8 w-8" />
+								</a>
+							</div>
+						</div>
 					</div>
-					<div class="card__info">
-						<h1></h1>
-						<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat quia non quam, aliquid
-							sapiente aliquam odit nulla repudiandae expedita est, cum, impedit molestiae suscipit ab
-							cumque! Corporis officia nostrum atque?</p>
+				</div>
+				<!-- CARD DE PRECIOS -->
+				<div class="card-pricings hover:scale-102 transition-all  max-w-[35rem] h-[60rem] bg-linear-45 from-main-50 via-main-50 to-main shadow-md  rounded-4xl p-4"
+					data-services="">
+					<div class="card__info flex flex-col h-full ">
+						<h1 class="text-3xl font-black text-center text-main-600 mt-8">Plan basico</h1>
+						<h2 class="text-6xl font-black text-center text-main-700 mb-8">300.000 Cop</h2>
+						<div
+							class="card__info__meta flex-2 bg-main-600 pt-8 rounded-t-4xl rounded-b-2xl p-8 text-white-app text-[18px]">
+							<p class="text-pretty mb-10 ">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+								Corrupti ab
+								natus mollitia vero, non
+								voluptatem autem unde voluptas et aperiam?</p>
+							<ul class="mb-20">
+								<li>
+									<div class="flex gap-2  items-center">
+										<CheckCircleIcon class="h-8 w-8" />
+										<p>Servicio principal</p>
+									</div>
+								</li>
+								<li>
+									<div class="flex gap-2  items-center">
+										<CheckCircleIcon class="h-8 w-8" />
+										<p>Servicio principal</p>
+									</div>
+								</li>
+								<li>
+									<div class="flex gap-2  items-center">
+										<CheckCircleIcon class="h-8 w-8" />
+										<p>Servicio principal</p>
+									</div>
+								</li>
+								<li>
+									<div class="flex gap-2  items-center">
+										<XCircleIcon class="h-8 w-8" />
+										<p>Servicio principal</p>
+									</div>
+								</li>
+
+							</ul>
+
+							<div class="card__info__footer flex justify-center">
+								<a href="#" class="botton__link flex gap-2">Obtener plan
+									<ArrowUpRightIcon class="h-8 w-8" />
+								</a>
+							</div>
+						</div>
 					</div>
-					<div class="card__footer"></div>
+				</div>
+				<!-- CARD DE PRECIOS -->
+				<div class="card-pricings hover:scale-102 transition-all  max-w-[35rem] h-[60rem] bg-linear-45 from-main-50 via-main-50 to-main shadow-md  rounded-4xl p-4"
+					data-services="">
+					<div class="card__info flex flex-col h-full ">
+						<h1 class="text-3xl font-black text-center text-main-600 mt-8">Plan basico</h1>
+						<h2 class="text-6xl font-black text-center text-main-700 mb-8">300.000 Cop</h2>
+						<div
+							class="card__info__meta flex-2 bg-main-600 pt-8 rounded-t-4xl rounded-b-2xl p-8 text-white-app text-[18px]">
+							<p class="text-pretty mb-10 ">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+								Corrupti ab
+								natus mollitia vero, non
+								voluptatem autem unde voluptas et aperiam?</p>
+							<ul class="mb-20">
+								<li>
+									<div class="flex gap-2  items-center">
+										<CheckCircleIcon class="h-8 w-8" />
+										<p>Servicio principal</p>
+									</div>
+								</li>
+								<li>
+									<div class="flex gap-2  items-center">
+										<CheckCircleIcon class="h-8 w-8" />
+										<p>Servicio principal</p>
+									</div>
+								</li>
+								<li>
+									<div class="flex gap-2  items-center">
+										<CheckCircleIcon class="h-8 w-8" />
+										<p>Servicio principal</p>
+									</div>
+								</li>
+								<li>
+									<div class="flex gap-2  items-center">
+										<XCircleIcon class="h-8 w-8" />
+										<p>Servicio principal</p>
+									</div>
+								</li>
+
+							</ul>
+
+							<div class="card__info__footer flex justify-center">
+								<a href="#" class="botton__link flex gap-2">Obtener plan
+									<ArrowUpRightIcon class="h-8 w-8" />
+								</a>
+							</div>
+						</div>
+					</div>
 				</div>
 
 			</section>
@@ -34,6 +168,14 @@
 
 <script setup>
 import { ref } from 'vue';
+
+import { CheckCircleIcon } from "@heroicons/vue/20/solid";
+
+import { XCircleIcon } from "@heroicons/vue/20/solid";
+
+import { ArrowUpRightIcon } from "@heroicons/vue/20/solid";
+
+
 
 
 const princingsMasoTerapia = ref([
@@ -62,95 +204,33 @@ const princingsInfo = () => {
 </script>
 
 <style scoped>
-/* button de secciones*/
-.button,
-.button *,
-.button :after,
-.button :before,
-.button:after,
-.button:before {
-	border: 0 solid;
-	box-sizing: border-box;
+/* poner mas visualmente el precios más utilizado */
+.card-pricings:nth-child(2) {
+	transform: translateY(-5rem);
 }
 
-.button {
-	-webkit-tap-highlight-color: transparent;
-	-webkit-appearance: button;
-	background-color: var(--color-black-app);
-	background-image: none;
-	color: #fff;
+/* boton card precios */
+.botton__link {
+	padding-left: 3.3rem;
+	padding-right: 3.3rem;
+	padding-bottom: 1.6rem;
+	padding-top: 1.6rem;
+	border-radius: 999px;
+	background: var(--color-main-200);
+	border: none;
+	color: black;
+	font-family: inherit;
+	text-align: center;
 	cursor: pointer;
-	font-size: 100%;
-	font-weight: 600;
-	line-height: 1.5;
-	margin: 0;
-	-webkit-mask-image: -webkit-radial-gradient(var(--color-black-app), #fff);
-	text-transform: uppercase;
+	transition: 0.4s;
 }
 
-.button:disabled {
-	cursor: default;
+.botton__link:hover {
+	box-shadow: 7px 5px 56px -14px #C3D900;
 }
 
-.button:-moz-focusring {
-	outline: auto;
-}
-
-.button svg {
-	display: block;
-	vertical-align: middle;
-}
-
-.button [hidden] {
-	display: none;
-}
-
-.button {
-	border-radius: 99rem;
-	border-width: 2px;
-	min-width: 30.5rem;
-	overflow: hidden;
-	padding: 3rem 4rem;
-	position: relative;
-}
-
-
-.button span {
-
-	mix-blend-mode: difference;
-}
-
-.button:after,
-.button:before {
-	background: linear-gradient(90deg,
-			#fff 25%,
-			transparent 0,
-			transparent 50%,
-			#fff 0,
-			#fff 75%,
-			transparent 0);
-	content: "";
-	inset: 0;
-	position: absolute;
-	transform: translateY(var(--progress, 100%));
-	transition: transform 0.2s ease;
-}
-
-.button:after {
-	--progress: -100%;
-	background: linear-gradient(90deg,
-			transparent 0,
-			transparent 25%,
-			#fff 0,
-			#fff 50%,
-			transparent 0,
-			transparent 75%,
-			#fff 0);
-	z-index: -1;
-}
-
-.button:hover:after,
-.button:hover:before {
-	--progress: 0;
+.botton__link:active {
+	transform: scale(0.97);
+	box-shadow: 7px 5px 56px -10px #C3D900;
 }
 </style>
